c2f801ce56e6dd3fdec6981d63673b84052e613e
Veronika Dzhambazova <veronika.dzhambazova@abv.bg>
2016-06-04 10:34:11 +0300

    Adding prev and next slide functionality to level classes

--- app/src/main/java/cyril_and_methodius/bukvar/levels/level_2/Level_02.java
+++ app/src/main/java/cyril_and_methodius/bukvar/levels/level_2/Level_02.java
@@ -1,25 +1,25 @@
-package cyril_and_methodius.bukvar.levels.level_1;
+package cyril_and_methodius.bukvar.levels.level_2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 
 import cyril_and_methodius.bukvar.R;
-import cyril_and_methodius.bukvar.levels.level_2.Level_02;
+import cyril_and_methodius.bukvar.levels.level_3.Level_03;
 
-
-public class Level0100 extends AppCompatActivity {
+public class Level_02 extends AppCompatActivity {
     private Intent nextActivity;
     private GestureDetectorCompat gestureDetectorCompat;
 
     @Override
-    public void onCreate(final Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.level_1_alphabet);
-        this.nextActivity = new Intent(this, Level_02.class);
+        this.setContentView(R.layout.level_2_easy_words);
+        nextActivity = new Intent(this, Level_03.class);
         this.gestureDetectorCompat = new GestureDetectorCompat(this, new MyGestureListener());
     }
 
--- app/src/main/java/cyril_and_methodius/bukvar/levels/level_4/Level_04.java
+++ app/src/main/java/cyril_and_methodius/bukvar/levels/level_4/Level_04.java
@@ -1,6 +1,5 @@
-package cyril_and_methodius.bukvar.levels.level_1;
+package cyril_and_methodius.bukvar.levels.level_4;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -8,18 +7,17 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 
 import cyril_and_methodius.bukvar.R;
-import cyril_and_methodius.bukvar.levels.level_2.Level_02;
 
-
-public class Level0100 extends AppCompatActivity {
-    private Intent nextActivity;
+/**
+ * Created by Teo on 6/3/2016.
+ */
+public class Level_04 extends AppCompatActivity {
     private GestureDetectorCompat gestureDetectorCompat;
 
     @Override
-    public void onCreate(final Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.level_1_alphabet);
-        this.nextActivity = new Intent(this, Level_02.class);
+        setContentView(R.layout.level_4_sentences);
         this.gestureDetectorCompat = new GestureDetectorCompat(this, new MyGestureListener());
     }
 
@@ -36,7 +34,7 @@ public class Level0100 extends AppCompatActivity {
                                float velocityX, float velocityY) {
 
             if (event2.getX() < event1.getX()) {
-                startActivity(nextActivity);
+                //startActivity(nextActivity);
             }
             return true;
         }
