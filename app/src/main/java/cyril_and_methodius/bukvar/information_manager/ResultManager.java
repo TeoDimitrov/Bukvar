35d4d5b658f9a403da541ec41749ee8baec81da1
TeoDimitrov <teodor.dimitrov.90@gmail.com>
2016-06-05 10:21:09 +0300

    progress_bar_works

--- app/src/main/java/cyril_and_methodius/bukvar/information_manager/ResultManager.java
+++ app/src/main/java/cyril_and_methodius/bukvar/information_manager/ResultManager.java
@@ -5,8 +5,10 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
 
 import cyril_and_methodius.bukvar.R;
+import cyril_and_methodius.bukvar.applicataion_launcher.MainActivity;
 
 /**
  * Created by teo on 04/06/16.
@@ -16,6 +18,9 @@ public final class ResultManager extends AppCompatActivity {
     private static Class nextActivity;
     private static String resultType;
     private MediaPlayer mediaPlayer;
+    private ProgressBar progressBar;
+    private int progressStatus;
+
 
     public ResultManager() {
         super();
@@ -24,11 +29,7 @@ public final class ResultManager extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
+        //Set Result
         boolean isSuccess = ResultManager.resultType.equals("Success");
         boolean isFailure = ResultManager.resultType.equals("Failure");
         if (isSuccess) {
@@ -41,6 +42,15 @@ public final class ResultManager extends AppCompatActivity {
             this.startMediaPlayer();
         }
 
+        //Set Progress Bar
+        this.progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        this.updateProgressBar();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -50,17 +60,24 @@ public final class ResultManager extends AppCompatActivity {
         }, ResultManager.DELAY);
     }
 
-    private void startMediaPlayer() {
-        this.mediaPlayer.start();
-    }
-
-
-
     public static void setNextActivity(Class nextActivity) {
         ResultManager.nextActivity = nextActivity;
     }
 
     public static void setResultType(String resultType) {
         ResultManager.resultType = resultType;
+    }
+
+    private void startMediaPlayer() {
+        this.mediaPlayer.start();
+    }
+
+    private void setProgressStatus(int progressStatus) {
+        this.progressStatus = progressStatus;
+    }
+
+    public void updateProgressBar() {
+        this.setProgressStatus(MainActivity.getUser().getLevelOneCurrentPoints());
+        this.progressBar.setProgress(this.progressStatus);
     }
 }
